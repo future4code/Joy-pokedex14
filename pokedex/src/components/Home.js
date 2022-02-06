1713cdfdd407c87c6d4eceb3b0a766a07ccd7240
ViniciusLussaniToigo <viniciuslussani@gmail.com>
2022-02-05 21:26:43 -0300

    Estilizando os botões

--- pokedex/src/components/Home.js
+++ pokedex/src/components/Home.js
@@ -2,14 +2,7 @@ import React, {  useState,useEffect} from 'react';
 import styled from 'styled-components'
 import axios from 'axios';
 import {useHistory} from 'react-router-dom';
-// import {PokeDex} from './components/PokeDex'
 
-const SecaoPokemons = styled.div`
-display:flex;
-flex-direction:column;
-align-items:center;
-
-`
 export const Home = () => {
 
         const history = useHistory()
@@ -39,7 +32,7 @@ export const Home = () => {
         <SecaoPokemons>
 
        
-        <button onClick={vaParaPokedex}>Vá para a Pokedex</button>  
+        <BotaoEstilizado onClick={vaParaPokedex}>Vá para a Pokedex</BotaoEstilizado>  
 
 
         <h1>Lista de Pokemons</h1>
@@ -48,10 +41,11 @@ export const Home = () => {
           return (
 
             
-            <p key={poke.name} >{poke.name}</p>
+            <div key={poke.name} >{poke.name}
+              {/* <img alt={pokemon.name} src={pokemon.sprites.other.official-artwork.front_default}/> */}
            
           
-            /* <img src={data.sprites["front_default"]}/> */
+              </div>
             
           )
         })}
@@ -59,3 +53,37 @@ export const Home = () => {
         </SecaoPokemons>
     );
 }
+
+
+const BotaoEstilizado=styled.button`
+margin-top:25px;
+background: #fa9e00;
+background-image: -webkit-linear-gradient(top, #fa9e00, #2076ab);
+background-image: -moz-linear-gradient(top, #fa9e00, #2076ab);
+background-image: -ms-linear-gradient(top, #fa9e00, #2076ab);
+background-image: -o-linear-gradient(top, #fa9e00, #2076ab);
+background-image: linear-gradient(to bottom, #fa9e00, #2076ab);
+-webkit-border-radius: 28;
+-moz-border-radius: 28;
+border-radius: 28px;
+font-family: Courier New;
+color: #fcfcfc;
+font-size: 20px;
+padding: 10px 20px 10px 20px;
+text-decoration: none;
+cursor:pointer;
+&:hover,
+&:focus {
+  color: yellow;
+}
+&:active {
+  color: white;
+}
+`
+
+const SecaoPokemons = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+
+`
